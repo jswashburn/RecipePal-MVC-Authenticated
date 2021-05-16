d8f6e82ad9f5a793e145b613941f99aa3c609b16
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-05-16 11:06:05 -0400

    add custom username/email policy

---
+++ RecipePal/IdentityPolicy/RecipePalUserPolicy.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Identity;
+using RecipePal.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipePal.IdentityPolicy
+{
+    public class RecipePalUserPolicy : UserValidator<AppUser>
+    {
+        public override async Task<IdentityResult> ValidateAsync(
+            UserManager<AppUser> userManager, AppUser appUser)
+        {
+            IdentityResult validationResult = await base.ValidateAsync(userManager, appUser);
+            var validationErrors = validationResult.Succeeded ?
+                new List<IdentityError>() : validationResult.Errors.ToList();
+
+            if (appUser.UserName == "wwishy")
+                validationErrors.Add(new IdentityError
+                {
+                    Description = "Can't have that name its already mine >:("
+                });
+
+            if (appUser.NormalizedEmail.EndsWith(".boogle"))
+                validationErrors.Add(new IdentityError
+                {
+                    Description = "Can't use .boogle emails"
+                });
+
+            return validationErrors.Count == 0 ?
+                IdentityResult.Success : IdentityResult.Failed(validationErrors.ToArray());
+        }
+    }
+}
