170d1f7c55e55f8158505ee907eaadb92012a2ad
jswashburn <77169258+jswashburn@users.noreply.github.com>
2021-05-18 11:47:40 -0400

    add dbsets

--- RecipePal/Data/AppIdentityDbContext.cs
+++ RecipePal/Data/AppIdentityDbContext.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using RecipePal.Models;
 using RecipePal.Models.Identity;
 
 namespace RecipePal.Data
 {
     public class AppIdentityDbContext : IdentityDbContext<AppUser>
     {
-        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
-            : base(options) { }
+        public DbSet<Cookbook> Cookbooks { get; set; }
+        public DbSet<Recipe> Recipes { get; set; }
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options) : base(options) 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) => modelBuilder.Seed();
     }
 }
